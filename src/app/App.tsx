81f8ef7b67c64a164af469a0a78f300ab604547a
aris1377 <bek77519101@gmail.com>
2024-09-16 10:16:31 +0900

    feat: create routing system

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,51 @@
 import React from "react";
 import "../css/app.css";
+import { Link, Route, Switch, useRouteMatch } from "react-router-dom";
 
-function App() { 
+function App() {
   return (
-    <div>HOME</div>
+     <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+
+      <Switch>
+        <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+        </Switch>
+      </div>
+
   );
 }
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { store } from './app/store';
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./app/MaterialTheme";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./app/App";
 import reportWebVitals from './reportWebVitals';
 import './css/index.css';
@@ -17,7 +18,9 @@ root.render(
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-          <App />
+         <Router>
+          <App/>
+        </Router>
       </ThemeProvider>
     </Provider>
   </React.StrictMode>
