12c79605327eaeaf1b1e000270d2c1922be08a09
aris1377 <bek77519101@gmail.com>
2024-09-16 22:44:22 +0900

    feat: develop ActiveUsers sectional components

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,23 +1,23 @@
 import React from "react";
 import { Container } from "@mui/material"
 import Statistics from "./Statistics";
-// import NewDishes from "./NewDishes";
+import NewDishes from "./NewDishes";
 // import Events from "./Events";
-// import ActiveUsers from "./ActiveUsers";
-// import Advertisement from "./Advertisement";
-// import PopularDishes from "./PopularDishes";
+import ActiveUsers from "./ActiveUsers";
+import Advertisement from "./Advertisement";
+import PopularDishes from "./PopularDishes";
 import "../../../css/home.css";
 
 export default function HomePage() {
    return (
     <div className={"homepage"}>
       <Statistics />
-      {/* <ActiveUsers />
+      <ActiveUsers />
       <PopularDishes />
       <NewDishes />
       <Advertisement />
       <ActiveUsers />
-      <Events /> */}
+      {/* <Events /> */}
     </div>
   );
 }
