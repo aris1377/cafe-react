83f5a55b3886fd89c7d4878823a4a82cf22ddc16
aris1377 <bek77519101@gmail.com>
2024-09-16 23:16:52 +0900

    feat: develop productPage mested routing system

--- src/app/screens/productsPage/index.tsx
+++ src/app/screens/productsPage/index.tsx
@@ -1,5 +1,28 @@
-import {Container} from "@mui/material"
+import React from "react";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
+import { Container } from "@mui/material";
+import ChosenProduct from "./ChosenProduct";
+import Products from "./Products";
+import "../../../css/products.css";
+import { CartItem } from "../../../lib/types/search";
 
-export default function ProductsPage() {
-  return <Container>ProductsPage</Container>;
+interface ProductsPageProps{
+  onAdd: (item: CartItem) => void;
+}
+export default function ProductsPage(props: ProductsPageProps) {
+  const { onAdd} = props;
+  const products = useRouteMatch();
+  console.log("test11:", products);
+  return (
+    <div className="products-page">
+      <Switch>
+        <Route path={`${products.path}/:productId`}>
+          <ChosenProduct onAdd={onAdd} />
+        </Route>
+        <Route path={`${products.path}`}>
+          <Products onAdd={onAdd} />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
