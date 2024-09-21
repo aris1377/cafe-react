b8cb5d6c8fc045a8c8a2140999b9d4e1e5ea5f67
aris1377 <bek77519101@gmail.com>
2024-09-21 16:25:01 +0900

    feat: create config.ts

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,27 +1,68 @@
 import React, { useEffect } from "react";
-import { Container } from "@mui/material"
 import Statistics from "./Statistics";
-import NewDishes from "./NewDishes";
-import Events from "./Events";
-import ActiveUsers from "./ActiveUsers";
-import Advertisement from "./Advertisement";
 import PopularDishes from "./PopularDishes";
+import NewDishes from "./NewDishes";
+import Advertisement from "./Advertisement";
+import ActiveUsers from "./ActiveUsers";
+import Events from "./Events";
+import { useDispatch } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { setNewDishes, setPopularDishes, setTopUsers } from "./slice";
+import { Product } from "../../../lib/types/product";
+import ProductService from "../../services/ProductService";
+import { ProductCollection } from "../../../lib/enums/product.enum";
+import MemberService from "../../services/MemberService";
+import { Member } from "../../../lib/types/member";
 import "../../../css/home.css";
 
-export default function HomePage() {
-  // Select: Store => Data
+//** REDUX SLICE & SELECTOR */
+/** SAVE TO SLISE **/
+const actionDispatch = (dispatch: Dispatch) => ({
+  setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+  setNewDishes: (data: Product[]) => dispatch(setNewDishes(data)),
+  setTopUsers: (data: Member[]) => dispatch(setTopUsers(data)),
+});
 
+export default function HomePage() {
+  const { setPopularDishes, setNewDishes, setTopUsers } = actionDispatch(
+    useDispatch()
+  );
+
+  // Select: Store => Data
   useEffect(() => {
-  // Backend server data request  => DATA
-    
-    
-  // Slice: Data => Srore
-   },[])
-   
-   return (
+    // Backend server data request  => DATA
+    const product = new ProductService();
+    product
+      .getProducts({
+        page: 1,
+        limit: 4,
+        order: "productViews",
+        productCollection: ProductCollection.COFFEE,
+      })
+      .then((data) => {
+        setPopularDishes(data);
+      })
+      .catch((err) => console.log(err));
+
+    product
+      .getProducts({
+        page: 1,
+        limit: 4,
+        order: "createAt",
+      })
+      .then((data) => setNewDishes(data))
+      .catch((err) => console.log(err));
+
+    const member = new MemberService();
+    member
+      .getTopUsers()
+      .then((data) => setTopUsers(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
     <div className={"homepage"}>
       <Statistics />
-      <ActiveUsers />
       <PopularDishes />
       <NewDishes />
       <Advertisement />
