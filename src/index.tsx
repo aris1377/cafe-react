741fc9033a7ab988ebdbbc86ef9c50eb9a7c13e9
aris1377 <bek77519101@gmail.com>
2024-12-23 19:02:06 +0900

    feat: deveop Socket.io Client

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,10 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./app/MaterialTheme";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./css/index.css";
 import ContextProvider from "./app/context/ContextProvder";
+import { SocketProvider } from "./app/context/SocketContext";
+import "./css/index.css";
+
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -18,12 +20,14 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <ContextProvider>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Router>
-            <App />
-          </Router>
-        </ThemeProvider>
+        <SocketProvider>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Router>
+              <App />
+            </Router>
+          </ThemeProvider>
+        </SocketProvider>
       </ContextProvider>
     </Provider>
   </React.StrictMode>
